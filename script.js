8b3889256994e8f268f4c1811fb6c9b7a6dd729f
emi-ak <298037097+emi-ak@users.noreply.github.com>
2026-07-01 17:12:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,36 +30,6 @@ function averageOfModuleTotals() {
   return average.toFixed(1);
 }
 
-const sidebar = document.getElementById("sidebar");
-document.getElementById("mobileMenu").addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-});
-
-function openPage(pageName) {
-  document.querySelectorAll(".nav-link").forEach(b => b.classList.remove("active"));
-  document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
-
-  const navButton = document.querySelector(`.nav-link[data-page="${pageName}"]`);
-  const page = document.getElementById(pageName);
-
-  if (!page) return;
-
-  if (navButton) {
-  navButton.classList.add("active");
-  }
-  
-  page.classList.add("active");
-  sidebar.classList.remove("open");
-
-  localStorage.setItem("emsPlannerCurrentPage", pageName);
-}
-
-document.querySelectorAll(".nav-link").forEach(button => {
-  button.addEventListener("click", () => {
-    openPage(button.dataset.page);
-  });
-});
-
 document.getElementById("categoryForm").addEventListener("submit", e => {
   e.preventDefault();
   data.categories.push({
