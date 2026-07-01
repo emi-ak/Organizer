f448b081937963f424b58b3b6aeea30a1c7aa711
emi-ak <298037097+emi-ak@users.noreply.github.com>
2026-07-01 17:11:24 -0400

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,31 @@
+function percent(done, goal) {
+  if (!goal) return 0;
+  return Math.min(100, Math.round((done / goal) * 100));
+}
+
+function totalHours(category) {
+  return category.activities.reduce((sum, a) => sum + Number(a.hours || 0), 0);
+}
+
+function escapeHTML(value) {
+  return String(value ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;");
+}
+
+function safeConfirmDelete(message = "Are you sure you want to delete this?") {
+  return confirm(message);
+}
+
+function toggleEdit(id) {
+  const el = document.getElementById(id);
+  if (el) el.classList.toggle("open");
+}
+
+function removeById(collection, id) {
+  if (!confirm("Are you sure you want to delete this?")) return;
+  data[collection] = data[collection].filter(item => item.id !== id);
+  save();
+}
